6a14f0c63b615c93640306b11c068fcdef3da3e1
Maxime <leroymax2006@gmail.com>
2022-05-10 21:51:57 +0200

    ok

--- index.js
+++ index.js
@@ -4,3 +4,35 @@ async function getMovies() {
     const data = await response.json()
     return data.Search
 }
+
+
+const data = getMovies().then(function (movies) {
+
+
+    console.log(movies)
+    movies.forEach(function (movie) {
+        console.log(movie.Title)
+    })
+
+    movies.forEach(function (movie) {
+        console.log(movie.Year)
+    })
+
+    movies.forEach(function (movie) {
+        const divMovieTitle = document.createElement("div")
+        divMovieTitle.textContent = movie.Title
+        document.body.appendChild(divMovieTitle)
+    })
+
+    movies.forEach(function (movie) {
+        const divMovieYear = document.createElement("div")
+        divMovieYear.textContent = movie.Year
+        document.body.appendChild(divMovieYear)
+    })
+
+    movies.forEach(function (movie) {
+        const img = document.createElement("img")
+        img.setAttribute("src", movie.Poster)
+        document.body.appendChild(img)
+    })
+}) 
