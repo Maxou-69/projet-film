d230127c9b02dcf5ab207f480167ea9358b6ca69
Maxou <96335174+Maxou-69@users.noreply.github.com>
2022-05-14 10:31:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 async function getMovies() {
-    const url = "api link delete for securitie"
+    const url = "https://www.omdbapi.com/?s=star%20wars&plot=short&apikey=2a4831ba"
     const response = await fetch(url)
     const data = await response.json()
     return data.Search
